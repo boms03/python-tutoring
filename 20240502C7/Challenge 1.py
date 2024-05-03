60c752217f230ddecdfed32a47a784b91357c36c
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-05-02 17:17:01 -0700

    20240502: This is my homework!

---
+++ 20240502C7/Challenge 1.py
@@ -0,0 +1,10 @@
+statement = []
+statement.append("yoonha")
+if True:
+        statement.append("studies")
+if True:
+    statement.append("python")
+if True:
+    statement.append("everyday")
+print(statement)
+    
