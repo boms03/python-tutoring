4824200b735d4a8763bb93cc96fb010ec950edca
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-05-10 17:39:42 -0700

    5/10/24: I fixed my homework

--- 20240510C8/Challenge #3.py
+++ 20240510C8/Challenge #3.py
@@ -1,10 +1,11 @@
 def sayhi():
     print("Hello! My name is Yoonha!")
-sayhi()
 def introduce():
     print("This is a little bit about me!")
+def favoriteNumber(n):
+    print("My favorite number is "+ n)
+sayhi()
 introduce()
-def favoriteNumber():
-    print("My favorite number is 4!")
-favoriteNumber(
+i = input()
+favoriteNumber(i)
 
