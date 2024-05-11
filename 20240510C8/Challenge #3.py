be42008d0361d9ba3e46904bc57f6c9e13edc912
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-05-10 17:07:45 -0700

    5/10/2024: This is my homework

---
+++ 20240510C8/Challenge #3.py
@@ -0,0 +1,10 @@
+def sayhi():
+    print("Hello! My name is Yoonha!")
+sayhi()
+def introduce():
+    print("This is a little bit about me!")
+introduce()
+def favoriteNumber():
+    print("My favorite number is 4!")
+favoriteNumber(
+
