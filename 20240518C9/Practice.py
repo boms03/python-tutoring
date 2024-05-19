613489902777e9b02c074ad0c32336e40c37432e
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-05-18 17:21:27 -0700

    5/18/24: I fixed it

--- 20240518C9/Practice.py
+++ 20240518C9/Practice.py
@@ -1,9 +1,7 @@
 foodlist = ['tacos']
-1=='order'
-2=='exit'
 while True:
     i=input()
     if i=='1':
-        print("you ordered" + str[foodlist])
+        print("you ordered " + foodlist[0])
     if i == '2':
-        print('exit')
+        break
