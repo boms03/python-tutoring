ad79668c5cf678b35ddc51d79521263b1f09e68e
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-06-15 18:03:41 -0700

    6/15/24: This is my classwork

---
+++ 20240616C12/Classwork 12.py
@@ -0,0 +1,14 @@
+n= input()
+print(n*2)
+
+n=int(input())
+print(n*2)
+
+n=input().split()
+print(n)
+
+map(int,input().split())
+'''map(int,"3 6 9".split())
+map(int, ["3" "6" "9"])
+[int("3"),int("6"),int("9")]
+[3, 6, 9]'''
