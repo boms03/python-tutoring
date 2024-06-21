9dedbf080ca2310a79176fef168df4653450577c
boms03 <beomsuyoo@gmail.com>
2024-06-22 08:44:51 +0900

    Create Classwork 13.py

---
+++ 20240621C13/Classwork 13.py
@@ -0,0 +1,14 @@
+#for loop
+
+foodList = ["pizza", "sushi", "pasta"]
+
+# Problem1: for loop foodList and print each
+
+# Problem2: for loop range from 3 to 6 by step 4
+# range(a,b) -> from a to b-1
+# range(a,b,c) -> from a to b-1 by step 2
+
+
+# Problem3: for loop range from 0 to 30 and print even numbers only
+
+        
