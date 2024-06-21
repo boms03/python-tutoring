cb36d538136f674e368079651e14382148108f70
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-06-21 16:52:02 -0700

    6/21/24 This is my classwork

--- 20240621C13/Classwork 13.py
+++ 20240621C13/Classwork 13.py
@@ -3,12 +3,17 @@
 foodList = ["pizza", "sushi", "pasta"]
 
 # Problem1: for loop foodList and print each
+for food in foodList:
+    print(food)
 
-# Problem2: for loop range from 3 to 6 by step 4
+# Problem2: for loop range from 3 to 30 by step 4
 # range(a,b) -> from a to b-1
 # range(a,b,c) -> from a to b-1 by step 2
-
+for i in range(3,30,4):
+    print(i)
 
 # Problem3: for loop range from 0 to 30 and print even numbers only
 
-        
+for i in range(0,30):
+    if (i%2==0):
+        print(i)
