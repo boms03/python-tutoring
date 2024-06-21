3ee1ac5cd6f936d53f1dce18f19c7f2f06b87c83
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-06-21 16:05:18 -0700

    5/21/24 This is my homework (12)

--- 20240616C12/Classwork 12.py
+++ 20240616C12/Classwork 12.py
@@ -1,11 +1,23 @@
 n= input()
 print(n*2)
+'''n="13"
+print("13"*2)
+"36"'''
 
 n=int(input())
 print(n*2)
+'''n=int("32"))
+n=32
+print(32*2)
+64'''
+
 
 n=input().split()
 print(n)
+'''n=23.split()
+n="23"
+print("23")
+"23"'''
 
 map(int,input().split())
 '''map(int,"3 6 9".split())
