6df9c84d33a35306137bd2df2f8eb9e181ef5065
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-07-05 17:58:35 -0700

    feat: added tip logic

--- 20240705C15/Cafe Homework.py
+++ 20240705C15/Cafe Homework.py
@@ -1,4 +1,4 @@
-Bankaccount = 10
+Bankaccount = 100
 Cafeaccount = 550
 foodlist = ['sandwhich', 'tea', 'coffee', 'cookie', 'lemonade']
 price_dict = {"sandwhich":20,"tea":8,"coffee":10,"cookie":5, "lemonade":7}    
@@ -7,6 +7,7 @@ print('Hello welcome to Yoonha\'s Calming Cafe')
 while True:
     print('Current Bankaccount is ' + str(Bankaccount))
     print('Current Cafeacccount is ' + str(Cafeaccount))
+    print('Current orderedlist is ' + str(orderedlist))
     i=input()
     if i=='1':
         print('What would you like to order?')
@@ -22,13 +23,16 @@ while True:
                 print('I\'m sorry but you don\'t have enough') 
         else:
             print('Sorry, we don\'t have that here')
-
-
-
-
-
-
-            
     elif i=='2':
+        print('How much tip would you like to give?')
+        tip=int(input())
+        if Bankaccount>=tip:
+            print('You gave ' + str(tip))
+            Bankaccount=Bankaccount-tip
+            Cafeaccount=Cafeaccount+tip
+        orderedlist.clear()
+        print('Thank you for coming')
+        break
+    elif i=='3':
         print('Bye bye, see you soon')
         break
