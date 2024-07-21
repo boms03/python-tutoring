8e62b9fa76f1a86a98c160de1d166c1b84f5fb85
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-07-20 18:05:30 -0700

    fix: fixed coupon

--- 20240705C15/Cafe Homework.py
+++ 20240705C15/Cafe Homework.py
@@ -1,15 +1,13 @@
-def subtractMoney(Bankaccount,price,number):
-    return(Bankaccount-price*number)
-def addMoney(Cafeaccount,price,number):
-    return(Cafeaccount+price*number)
-def addOrder(orderedlist,food,number):
-    return(orderedlist+food*number)
+def subtractMoney(Bankaccount,price):
+    return(Bankaccount-price)
+def addMoney(Cafeaccount,price):
+    return(Cafeaccount+price)
 Bankaccount = 100
 Cafeaccount = 550
 foodlist = ['sandwhich', 'tea', 'coffee', 'cookie', 'lemonade']
 price_dict = {"sandwhich":20,"tea":8,"coffee":10,"cookie":5, "lemonade":7}
 couponlist = [5,10, 15]
-orderedlist=def addOrder(orderedlist,food,number)
+orderedlist=[]
 print('Hello welcome to Yoonha\'s Calming Cafe')
 while True:
     print('Current Bankaccount is ' + str(Bankaccount))
@@ -25,13 +23,29 @@ while True:
             print('How many would you like to order?')
             number=int(input())
             if Bankaccount>=price_dict[food]*number:
+                price=price_dict[food]*number
+                while True: 
+                    print('Would you like to use a coupon?')
+                    letter=input()
+                    if letter == 'y':
+                        print('Which one would you like to use?' + str(couponlist))
+                        coupon=int(input())
+                        if coupon in couponlist:
+                            price=price*(100-coupon)/100
+                        else:
+                            print('I\'m sorry but you don\'t have that coupon')
+                        break
+                    elif letter =='n':
+                        print('Okay, you are not using a coupon')
+                        break
+                    else:
+                        print('please answer with y or n')
                 for i in range(number):
                     orderedlist.append(food)
-                    Bankaccount=def subtractMoney(Bankaccount,price,number)
-                    Cafeaccount=def addMoney(Cafeaccount,price,number)
-                    print("You have ordered " + str(number) +' '+ food)
-
-                
+                Bankaccount=subtractMoney(Bankaccount,price)
+                Cafeaccount=addMoney(Cafeaccount,price)
+                print("You have ordered " + str(number) +' '+ food)
+                print('price:'+str(price))
             else:
                 print('I\'m sorry but you don\'t have enough') 
         else:
@@ -39,18 +53,12 @@ while True:
 
 
 
-    elif i=='2':
-        print('How much would you like to use?')
-        number=int(input())
-        if number in couponlist:
-            Bankaccount=Bankaccount+number
-            Cafeaccount=Cafeaccount-number
     elif i=='3':
         print('How much tip would you like to give?')
         tip=int(input())
         if Bankaccount>=tip:
-            Bankaccount=Bankaccount-tip
-            Cafeaccount=Cafeaccount+tip
+            Bankaccount=subtractMoney(Bankaccount,tip)
+            Cafeaccount=addMoney(Cafeaccount,tip)
             print('You gave ' + str(tip))
             orderedlist.clear()
             print('Thank you for coming')
