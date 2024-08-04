c74fb95ac593377420018f84a340cdabb83db486
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-08-03 18:26:11 -0700

    feat: create, read, and update todo's

--- 20240727C16/ToDoList Project.py
+++ 20240727C16/ToDoList Project.py
@@ -7,13 +7,56 @@ class ToDo:
     def __lt__(self,other):
         return self.priority>other.priority
 
-ToDo1=ToDo('Homework', 'python', False, 1)
-ToDo2=ToDo('Memorize', 'sadlier', False, 2)
-ToDo3=ToDo('Study', 'spanish', False,3)
-ToDo4=ToDo('Solve', 'math', False,3)
-ToDo5=ToDo('Typing', 'Writing', False, 2)
+ToDoList=[]
+while True:
+    print("Press 1, make your own todo.\nPress 2, show all your todo.\nPress 3, show your finsihed todo.\nPress 4, show your unfinsihed todo.")
+    i = input()
+    if i == '1':
+        print("Make a todo:")
+        title = input("title:")
+        description = input("description:")
+        isDone = input("Is done?:")
+        priority = input("priority:")
+        ToDo1=ToDo(title,description,isDone,priority)
+        ToDoList.append(ToDo1)
+        print("Successfully added a new todo")
+        ToDoList.sort()
 
-ToDoList=[ToDo1, ToDo2,ToDo3, ToDo4, ToDo5] 
-ToDoList.sort()
-for ToDo in ToDoList:
-    print(ToDo.priority, ToDo.title)
+    elif i == '2':
+        print("Showing all todo \n")
+        for todo in ToDoList:
+            print(todo.priority, todo.title, todo.description, todo.isDone)
+        print()
+
+
+    elif i == '3':
+        print("Finished todo \n")
+        for todo in ToDoList:
+            if todo.isDone == 'yes':
+                print(todo.priority, todo.title, todo.description, todo.isDone)
+        print()
+
+
+    elif i == '4':
+        print("Work on todo \n")
+        for todo in ToDoList:
+            if todo.isdone == 'no':
+                print(todo.priority, todo.title, todo.description, todo.isDone)
+        print()
+
+    elif i == '5':
+        print("Update your todo \n")
+        for index, todo in enumerate(ToDoList):
+              print(index,todo.priority, todo.title, todo.description, todo.isDone)
+        index = int(input("Which index will you update?"))
+        p = input()
+        if p == 'a':
+            newTitle = input("New title:")
+            todo = ToDoList[index]
+            todo.title = newTitle
+            
+
+                    
+
+
+              
