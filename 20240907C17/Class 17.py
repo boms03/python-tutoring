f85d08c701a9b2550f6e3735661d3880f7b1c319
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-09-07 18:18:04 -0700

    feat: for and while loop

---
+++ 20240907C17/Class 17.py
@@ -0,0 +1,67 @@
+my_list = [1,2,3,4, 5, 6, 7, 8, 9, 10]
+'''for i in my_list:
+    print(i)
+index=0
+while index<10:
+    print(my_list[index])
+    index = index+1
+index=0   
+while True:
+    if index<10:
+        print(my_list[index])
+        index = index+1
+    else: break'''
+  
+'''for i in range(1,11):
+    print(i)
+i=1
+while True:
+    if i<10:
+        print(i)
+        i=i+1
+    else:break
+
+i=1
+while i<10:
+    print(i)
+    i=i+1'''
+
+'''for i in range(1,11):
+    if i%2==0:
+        print(i)'''
+'''i=1
+while True:
+    if i==11:
+        break
+    if i%2==0:
+        print(i)
+    i=i+1'''
+   
+
+'''for i in range (0,11,2):
+    print(i)
+i=1
+while i<11:
+    if i%2==0:
+        print(i)
+    i=i+1'''
+    
+
+
+for index, value in enumerate(my_list):
+    print(index, value)
+i=0
+while True:
+    if i==10:
+        break
+    print(i, my_list[i])
+    i=i+1
+
+i=0
+while i<10:
+    print(i, my_list[i])
+    i=i+1
+    
+
+
+    
