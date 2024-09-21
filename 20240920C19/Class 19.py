ed5afceea9aa140402b6519ced8afb952450ff2d
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-09-20 17:56:45 -0700

    feat: map, while loop

---
+++ 20240920C19/Class 19.py
@@ -0,0 +1,32 @@
+'''fruitlist=[]
+while True:
+    i=int(input())
+    if i==3:
+        break
+    if i==1:
+        fruits=input().split()
+        fruitlist.extend(fruits)
+    if i==2:
+        for e in fruitlist:
+            print(e)'''
+
+
+
+'''for i in range(11):
+    if i%2==1:
+        print(i)
+i=0
+while True:
+    if i==11:
+        break
+    else:
+        print(i)
+    i=i+1'''
+'''i=0
+while i<11:
+    if i%2==0:
+        print(i)
+    i=i+1'''
+
+
+numberList = map(int, input().split())
