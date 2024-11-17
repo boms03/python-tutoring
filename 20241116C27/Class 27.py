e2c03ac24f76586cfc04bb20803821d98dda5b08
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2024-11-16 17:10:04 -0800

    feat:create tree and add nodes

---
+++ 20241116C27/Class 27.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.child=[]
+
+class Tree:
+    def __init__(self,root):
+        self.root=root
+
+
+newNode=Node(4)
+newTree=Tree(newNode)
+
+newNode2=Node(2)
+newNode3=Node(5)
+newNode4=Node(9)
+
+newTree.root.child.append(newNode2)
+newTree.root.child.append(newNode3)
+newTree.root.child.append(newNode4)
+
+newNode5=Node(4)
+newNode6=Node(1)
+
+newNode3.child.append(newNode5)
+newNode3.child.append(newNode6)
