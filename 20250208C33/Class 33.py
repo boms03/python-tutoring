ad099c1ba8f7f3cd4f0cf189e30e433a0c0b967e
EvieLand747 <168789932+EvieLand747@users.noreply.github.com>
2025-02-08 17:40:08 -0800

    feat:Tree recursion

---
+++ 20250208C33/Class 33.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,value,right,left):
+        self.val=value
+        self.right=right
+        self.left=left
+class Tree:
+    def __init__(self,root):
+        self.root=root
+    def preorder(self,node):
+        if not node:
+            return
+        print(node.value)
+        self.preorder(node.left)
+        self.preorder(node.rigt)
+    def inorder(self,node):
+        if not node:
+            return
+        self.inorder(node.left)
+        print(node.value)
+        self.inorder(node.right)
+    def postorder(self,node):
+        if not node:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        print(node.value)
+        
+
+newNode=Node(5,None,None)
+newTree=Tree(newNode)
+    
